1b156854fbc8758fde1ac2006cd061f9101b1cb0
Derek Sizemore <39501745+dereksizemore@users.noreply.github.com>
2022-04-21 14:55:01 -0500

    Standardized code/Added Comments

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
+/* DECLARATIONS */
 const textHeadMain = document.querySelector('#enter-button');
 const fgBodyC2 = document.querySelector('#fg-body-c2');
 const logo = document.querySelector("#head-logo-img");
 const loadScript = document.createElement('script');
 
-function openPage(){
-	fgBodyC2.innerHTML = mainBody;
-	document.querySelector('body').appendChild(loadScript)
-	loadScript.setAttribute('src', 'mainScript.js');
-	loadScript.setAttribute('type','text/javacsript');
-}
+/* Event Listerens */
+
+/* Enter Main Page */
 textHeadMain.addEventListener('mouseup',event =>{
 	location.href = "main.html";
-})
-
-logo.addEventListener('mouseup',event =>{
-	location.href = "index.html";
 })
