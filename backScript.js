3526285414da65c818c9216da99fc985d7139eed
Derek Sizemore <39501745+dereksizemore@users.noreply.github.com>
2022-04-20 21:41:32 -0500

    Added Image Navigation
    Added Javascript functions to draw arrows and allow for next and previous image navigation within each section.

--- backScript.js
+++ backScript.js
@@ -2,29 +2,58 @@ const backbut = document.querySelector('#back-container');
 const logo = document.querySelector('#head-logo-img');
 const portimg = document.querySelectorAll('.portimg');
 const overlay = document.querySelector('body')
-const div = document.createElement('div');
-const img = document.createElement('img');
-const p = document.createElement('p');
 
 function featureImg (imgEle)
 {
+	let div = document.createElement('div');
+	let img = document.createElement('img');
+	let p = document.createElement('p');
+
 	overlay.prepend(div);
 	div.setAttribute('id', 'focus-container');
+	div.setAttribute('class','no-hili');
 	div.prepend(img);
+
 	img.setAttribute('id', 'focus-img');
 	img.setAttribute('src', imgEle.src);
+
 	div.prepend(p);
 	p.setAttribute('id', 'focusExit');
-	p.setAttribute('class', 'no-hili');
 	p.innerHTML = 'X';
+
 	p.addEventListener('mouseup',event =>{
 		div.remove();
 	})
-}	
+	if (imgEle.nextElementSibling !== null)
+	{
+		p = document.createElement('p');
+		div.prepend(p);
+		p.setAttribute('id','next-arrow-p');
+		p.innerHTML = '>'
+		p.addEventListener('mouseup',event =>{
+			div.remove()
+			featureImg(imgEle.nextElementSibling)
+		})
+	}
+
+	if (imgEle.previousElementSibling !== null)
+	{
+		p = document.createElement('p');
+		div.prepend(p);
+		p.setAttribute('id','prev-arrow-p');
+		p.innerHTML = '<'
+		p.addEventListener('mouseup',event =>{
+			div.remove()
+			featureImg(imgEle.previousElementSibling)
+		})
+	}
+}
+
+
 
 portimg.forEach((item) =>{
 	item.addEventListener('mouseup',event =>{
-		featureImg(item);
+		overDiv = featureImg(item);
 	})
 })
 
